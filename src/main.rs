2bf6428b0212b72165e6b9081496ad572543d772
简 <2447746158@qq.com>
2022-12-03 00:09:42 +0800

    implement `cat-file`

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
+use flate2::read::ZlibDecoder;
+use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+
+    match args[1].as_str() {
+        "init" => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+        },
+        "cat-file" => {
+            let sha1_code = args[3].as_str();
+            let subdir = &sha1_code[0..2];
+            let filename = &sha1_code[2..];
+
+            let object = fs::read(format!(".git/objects/{}/{}", subdir, filename)).unwrap();
+            let mut decoder = ZlibDecoder::new(&object[..]);
+            let mut store = String::new();
+            decoder.read_to_string(&mut store).unwrap();
+
+            let content = store.splitn(2, '\0').collect::<Vec<&str>>()[1];
+            print!("{}", content);
+        },
+        _ => {
+            println!("unknown command: {}", args[1])
+        }
     }
 }
